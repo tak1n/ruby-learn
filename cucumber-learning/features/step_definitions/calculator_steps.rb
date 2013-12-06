316b23a73fbf7cf9ada0039bd0f04a7833f531cc
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-06 14:57:14 +0100

    testing with cucumber first secanrio passed

--- cucumber-learning/features/step_definitions/calculator_steps.rb
+++ cucumber-learning/features/step_definitions/calculator_steps.rb
@@ -1,11 +1,12 @@
-Given(/^the input "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^the input "(.*?)"$/) do |input|
+  @input = input
 end
 
 When(/^the calculator is run$/) do
-  pending # express the regexp above with the code you wish you had
+  @output = `ruby calculator.rb #{@input}`
+  raise('Command failed!') unless $?.success?
 end
 
-Then(/^the output should be "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^the output should be "(.*?)"$/) do |expected_output|
+  @output.should == expected_output
 end
