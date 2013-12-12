90d8870aaeb8321ea0ba6dc797c05be5aebeccc0
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-12 07:18:28 +0100

    added my_map and each2

--- yield.rb
+++ yield.rb
@@ -3,5 +3,32 @@ def parrot(argument)
 end
 
 parrot("Polly want a cracker.") do |argument|
-  puts "hi"
+  puts argument
 end
+
+class Array
+  def my_map
+    result = []
+    self.each do |item|
+      result << yield(item)
+    end
+    result
+  end
+
+  def each2   
+    i = 0;  
+    while self[i]  
+      yield self[i]  
+      i += 1  
+    end  
+    self  
+  end 
+end
+
+[1, 2, 3].my_map{|i| print i}
+print "\n"
+[2, 3, 5].each2{|i| print i}
+ 
+    
+# Use map when you need an new array returned
+# Use each when you need to loop through an array but don't necessarily neeed an new array
