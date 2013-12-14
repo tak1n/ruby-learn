eb39409f00317199bfa200efeb4d83071f425746
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-14 17:45:19 +0100

    added my proxy example

--- metaprogramming-book/DynamicProxy.rb
+++ metaprogramming-book/DynamicProxy.rb
@@ -21,3 +21,19 @@ proxy.my_awesome_method
 
 puts proxy.class
 # => Array
+
+
+class MyDynamicProxy
+  instance_methods.each { |m| undef_method m unless m =~ /(^__|^send$|^object_id$)/ }
+
+  def initialize(target)
+    @target = target
+  end
+
+  def method_missing(name, *args, &block)
+    @target.send(name, *args, &block)
+  end
+end
+
+obj = MyDynamicProxy.new({"first" => "first"})
+puts obj.class # => "result: gnirts a"
