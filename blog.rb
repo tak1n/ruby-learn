5f663c9a9a9d38713e4a1b3c1066a8ca51651a39
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-17 17:56:28 +0100

    method definition context

--- blog.rb
+++ blog.rb
@@ -4,21 +4,12 @@ class Object
 	end
 end
 
-
 class MyClass
-     def initialize
-         puts "hi im a new ruby object from the class MyClass"
-     end
-
-     def self
-     	self
-     end
 end
- 
-myobject = MyClass.new
-#puts myobject.self
-#puts myobject.class
-#puts myobject.class.superclass
-#puts myobject.class.superclass.superclass
 
-puts myobject.eigenclass
+class MySecondClass < MyClass
+end
+
+puts MySecondClass.eigenclass
+puts MySecondClass.eigenclass.superclass
+puts MySecondClass.superclass
