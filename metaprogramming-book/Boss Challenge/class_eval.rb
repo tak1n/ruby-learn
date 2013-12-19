85b1bf9d108b032d659eae9e48199e9bed72bca8
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-19 17:27:02 +0100

    source styling

--- metaprogramming-book/Boss Challenge/class_eval.rb
+++ metaprogramming-book/Boss Challenge/class_eval.rb
@@ -1,6 +1,6 @@
 
   def add_checked_attribute(klass, attribute)
-  	klass.class_eval do
+    klass.class_eval do
       define_method "#{attribute}=" do |value|
         raise 'Invalid attribute' unless value
         instance_variable_set("@#{attribute}", value)
