080b1c95313bc4b02c6fc34c63cb2a530af144a3
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-19 17:29:53 +0100

    source styling

--- metaprogramming-book/Boss Challenge/macro.rb
+++ metaprogramming-book/Boss Challenge/macro.rb
@@ -1,6 +1,6 @@
 class Class
   def attr_checked(attribute, &validation)
-  	define_method "#{attribute}=" do |value|
+    define_method "#{attribute}=" do |value|
       raise 'Invalid attribute' unless validation.call(value)
       instance_variable_set("@#{attribute}", value)
     end
