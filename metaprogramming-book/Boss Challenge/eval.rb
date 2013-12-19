77d2912fa7b144f2437bb7188866ce4d978d9e4c
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-19 17:28:07 +0100

    source styling

--- metaprogramming-book/Boss Challenge/eval.rb
+++ metaprogramming-book/Boss Challenge/eval.rb
@@ -1,15 +1,15 @@
 
   def add_checked_attribute(klass, attribute)
-  	eval "
-  	  class #{klass}
-  	    def #{attribute}
-  	      @#{attribute}
-  		end
-
-  		def #{attribute}=(value)
-  		  raise 'Invalid attribute' unless value
-  		  @#{attribute} = value
-  		end
+    eval "
+  	class #{klass}
+  	  def #{attribute}
+  	    @#{attribute}
       end
+
+  	  def #{attribute}=(value)
+        raise 'Invalid attribute' unless value
+        @#{attribute} = value
+  	  end
+    end
   	"
   end
