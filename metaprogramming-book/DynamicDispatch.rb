91cbf69b51a7ae228ead21562431967a3cf1799d
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-21 20:21:40 +0100

    Dynamic Dispatch

---
+++ metaprogramming-book/DynamicDispatch.rb
@@ -0,0 +1,11 @@
+class MyClass
+  def my_method(arg)
+    puts arg
+  end
+end
+
+obj = MyClass.new
+obj.my_method(3) # normal dot notation for method call
+obj.send(:my_method, 3) 
+# using this style the method gets a normal argument and u can wait to know your method call til the very last moment it gets called
+# This is called Dynamic Dispatch
