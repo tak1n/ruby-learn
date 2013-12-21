a68e0bbcfdaa3147f646c599e494cec2a0cc04f2
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-21 23:08:22 +0100

    flat scope

--- metaprogramming-book/FlatScope.rb
+++ metaprogramming-book/FlatScope.rb
@@ -9,3 +9,8 @@ MyClass = Class.new do
 end
 
 MyClass.new.my_method
+
+
+#If you replace Scope Gates with method calls, you allow one scope to see variables from another scope.
+#Technically, this trick should be called nested lexical scopes, but many Ruby coders refer to it simply as "flattening the scope"
+#Meaning that the two scopes share variables as if the scopes were squeezed together.
