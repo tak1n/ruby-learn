53da739262346a34c860ed2de9441ff8ec5e7373
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-21 19:58:17 +0100

    Kernel Methods

---
+++ metaprogramming-book/kernel.rb
@@ -0,0 +1,10 @@
+module Kernel
+  def kernel_method
+    puts "Kernel Method"
+  end
+end
+
+kernel_method
+
+#Spell Kernel Method
+#Methods looks like a language keyword because Kernel gets mixed in Object 
