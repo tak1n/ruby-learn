7ab46c61034794e37c92bf74ce30a8d2dcba3593
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-21 19:52:10 +0100

    OpenClass, MonkeyPatch

---
+++ metaprogramming-book/OpenClass.rb
@@ -0,0 +1,19 @@
+class String
+  def to_alphanumeric
+    gsub(/[^\w\s]/, '')
+  end
+end
+
+require 'test/unit'
+
+class StringExtensionsTest < Test::Unit::TestCase
+  def test_strips_non_alphanumeric_characters
+    assert_equal('3 the Magic Number', '#3, the *Magic ,Number*?'.to_alphanumeric)
+  end
+end
+
+
+#Spell Open Class
+#Monkey Patch is to use Open Class in the way to redefine a instance Method of class
+#For example: Array#replace
+#Sometimes it is useful but be carefull about this trick!!!
