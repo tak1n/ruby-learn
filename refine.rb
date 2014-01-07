b0e8f5b6cbcab6b17439240459a1a3bc352d9f49
Benny1992 <klotz.benjamin@yahoo.de>
2014-01-07 07:01:38 +0100

    ruby 2.1.0 feature - Refinements

---
+++ refine.rb
@@ -0,0 +1,16 @@
+module TimeExtensions
+  refine Fixnum do
+    def minutes; self * 60; end
+  end
+end
+
+class MyApp
+  using TimeExtensions
+
+  def initialize
+    p 2.minutes
+  end
+end
+
+MyApp.new    # => 120
+p 2.minutes
