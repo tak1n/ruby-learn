e4fb37d581c7627f2587aacecf7214004fd0fd0b
Benny1992 <klotz.benjamin@yahoo.de>
2014-01-30 11:18:27 +0100

    seperate Rakefiles for minitest and rspec

--- templates/test_helper.rb
+++ templates/test_helper.rb
@@ -1,2 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride' # for colored output
+require 'minitest/unit'
+
 require_relative '../lib/<path to bootstrap file>/<bootstrap.rb>'
-require 'test/unit'
+# alternative to require_relative
+# rake install in gem dir and then
+# require 'mygem'
+
