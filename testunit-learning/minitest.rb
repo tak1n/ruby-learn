85e93a315d6a24848d2356c9f2bca4c5b80f0c78
Benny1992 <klotz.benjamin@yahoo.de>
2014-01-30 11:41:11 +0100

    minitest example added

---
+++ testunit-learning/minitest.rb
@@ -0,0 +1,35 @@
+require 'minitest/autorun'
+ 
+class TestCashRegister < MiniTest::Unit::TestCase 
+  def setup
+    @register = CashRegister.new
+  end
+  def test_default_is_zero
+    assert_equal 0, @register.total
+  end
+  def test_total_calculation
+    @register.scan 1
+    @register.scan 2
+    assert_equal 3, @register.total
+  end
+  def test_total_is_zero_after_clear
+    @register.scan 1
+    @register.clear
+    assert_equal 0, @register.total
+  end
+end
+ 
+class CashRegister
+  def initialize
+    @items = []
+  end
+  def total
+    @items.inject(0, &:+)
+  end
+  def scan(item)
+    @items << item
+  end
+  def clear
+    @items = []
+  end
+end
