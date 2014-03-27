37f49902e1783a28dc6f6527b3848afa4b39048e
Benny1992 <klotz.benjamin@yahoo.de>
2014-03-27 22:27:33 +0100

    hello world ffi example

---
+++ ffi/helloffi/hello.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+int hellomath(int a, int b) {
+  printf("Hello from C land");
+  return a + b;
+}
+
---
+++ ffi/helloffi/hello.rb
@@ -0,0 +1,15 @@
+require 'ffi'
+
+module Hello
+  extend FFI::Library
+  ffi_lib "./libhello.so"
+  attach_function :hellomath, [:int, :int], :int
+
+  def self.math(a, b)
+    puts "Hello from Ruby land"
+    puts hellomath(a, b)
+  end
+end
+
+Hello.math(3, 5)
+
