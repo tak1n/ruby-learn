d74b3581baf90b85e8699b93faa638385b09f6bf
Benny <r3qnbenni@gmail.com>
2014-05-05 16:09:26 +0200

    validation testing

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test 'product is not valid without a unique title' do
+    product = Product.new(:title        => products(:one).title,
+                          :description  => 'yyy',
+                          :price        => 1,
+                          :image_url    => 'fred.gif')
+
+    assert !product.save
+    assert_equal [I18n.translate('errors.messages.taken' )],
+                 product.errors[:title]
+  end
 end
