45cc959f991c896de0091b60454da92ba17facdf
Benny <r3qnbenni@gmail.com>
2014-05-07 01:29:50 +0200

    add polymorphism

--- refactoring-ruby-edition/chapter1/good/movie.rb
+++ refactoring-ruby-edition/chapter1/good/movie.rb
@@ -3,26 +3,23 @@ class Movie
   NEW_RELEASE = 1
   CHILDRENS = 2
 
-  attr_reader :title
-  attr_accessor :price_code
+  attr_reader :title, :price_code
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def price_code=(value)
+    @price_code = value
+    @price = case @price_code
+    when REGULAR then RegularPrice.new
+    when NEW_RELEASE then NewReleasePrice.new
+    when CHILDRENS then ChildrensPrice.new
+    end
+  end
+
+  def initialize(title, the_price_code)
+    @title, self.price_code = title, the_price_code
   end
 
   def charge(days_rented)
-    result = 0
-
-    case @price_code
-    when REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when NEW_RELEASE
-      result += days_rented * 3
-    when CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
@@ -33,3 +30,25 @@ class Movie
     end
   end
 end
+
+class RegularPrice
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+  end
+end
+
+class NewReleasePrice
+  def charge(days_rented)
+    days_rented * 3
+  end
+end
+
+class ChildrensPrice
+  def charge(days_rented)
+    result = 1.5
+    result += (days_rented - 3) * 1.5 if days_rented > 3
+    result
+  end
+end
+
