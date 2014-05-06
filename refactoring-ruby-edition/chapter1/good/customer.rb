e8514bebb5cd5efe595857589edad91075d253e1
Benny <r3qnbenni@gmail.com>
2014-05-07 00:18:10 +0200

    refactor step - rename variables to communicate purpose

--- refactoring-ruby-edition/chapter1/good/customer.rb
+++ refactoring-ruby-edition/chapter1/good/customer.rb
@@ -14,19 +14,19 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
 
-    @rentals.each do |element|
-      this_amount = amount_for(element)
+    @rentals.each do |rental|
+      this_amount = amount_for(rental)
 
       # add frequent renter points
       frequent_renter_points += 1
 
       # add bonus for a two day new release rental
-      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+      if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
         frequent_renter_points += 1
       end
 
       # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
+      result += "\t" + rental.movie.title + "\t" + this_amount.to_s + "\n"
       total_amount += this_amount
     end
 
@@ -41,18 +41,18 @@ class Customer
   # This method was created by Extract Method from Customer#statement
   # https://github.com/Benny1992/RubyApplications/blob/master/refactoring-ruby-edition/chapter1/bad/customer.rb#L18-L30
   #
-  def amount_for(element)
-    this_amount = 0
+  def amount_for(rental)
+    result = 0
 
-    case element.movie.price_code
+    case rental.movie.price_code
     when Movie::REGULAR
-      this_amount += 2
-      this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
+      result += 2
+      result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
     when Movie::NEW_RELEASE
-      this_amount += element.days_rented * 3
+      result += rental.days_rented * 3
     when Movie::CHILDRENS
-      this_amount += 1.5
-      this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+      result += 1.5
+      result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
     end
   end
 end
