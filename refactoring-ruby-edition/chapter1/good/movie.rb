37f4957aad47a345e6a5a23fc07dfe472460e5f6
Benny <r3qnbenni@gmail.com>
2014-05-07 01:05:50 +0200

    refactor step - move case statement into movie class which data is used in case stmt

--- refactoring-ruby-edition/chapter1/good/movie.rb
+++ refactoring-ruby-edition/chapter1/good/movie.rb
@@ -9,4 +9,19 @@ class Movie
   def initialize(title, price_code)
     @title, @price_code = title, price_code
   end
+
+  def charge(days_rented)
+    result = 0
+
+    case @price_code
+    when REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when NEW_RELEASE
+      result += days_rented * 3
+    when CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+  end
 end
--- refactoring-ruby-edition/chapter1/good/rental.rb
+++ refactoring-ruby-edition/chapter1/good/rental.rb
@@ -14,18 +14,7 @@ class Rental
   # Old method - https://github.com/Benny1992/RubyApplications/blob/411524c483334c73d1b2dfd95112a3c0950983b1/refactoring-ruby-edition/chapter1/good/customer.rb#L40-L53
   #
   def charge
-    result = 0
-
-    case @movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (@days_rented - 2) * 1.5 if @days_rented > 2
-    when Movie::NEW_RELEASE
-      result += @days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (@days_rented - 3) * 1.5 if @days_rented > 3
-    end
+    @movie.charge(@days_rented)
   end
 
   def frequent_renter_points
