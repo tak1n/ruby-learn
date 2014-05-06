35a4700b4d7ddacd4bd900d120af8e9d1091ab29
Benny <r3qnbenni@gmail.com>
2014-05-07 00:31:39 +0200

    updated test example

--- refactoring-ruby-edition/chapter1/good/run.rb
+++ refactoring-ruby-edition/chapter1/good/run.rb
@@ -2,10 +2,34 @@ require_relative 'movie'
 require_relative 'rental'
 require_relative 'customer'
 
-movie = Movie.new('Aliens vs Predators', 1)
-rental = Rental.new(movie, 10)
+begin
+  movie_benny   = Movie.new('Aliens vs Predators', 0)
+  rental_benny  = Rental.new(movie_benny, 10)
 
-benny = Customer.new('benny')
-benny.add_rental(rental)
+  benny         = Customer.new('benny')
 
-puts benny.statement
+  benny.add_rental(rental_benny)
+  puts benny.statement
+end
+
+begin
+  movie_joe     = Movie.new('Aliens vs Predators 2', 1)
+  rental_joe    = Rental.new(movie_joe, 12)
+
+  joe           = Customer.new('joe')
+
+  joe.add_rental(rental_joe)
+  puts joe.statement
+end
+
+begin
+  movie_doe     = Movie.new('Aliens vs Predators 3', 2)
+  rental_doe    = Rental.new(movie_doe, 16)
+
+  doe           = Customer.new('doe')
+
+  doe.add_rental(rental_doe)
+  puts doe.statement
+end
+
+
