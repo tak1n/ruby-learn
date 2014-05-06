1defa3c0d5ccf0673a9034e6aae35ca5e3385338
Benny <r3qnbenni@gmail.com>
2014-05-07 00:50:39 +0200

    refactor step Replace Temp with Query on total_amount

--- refactoring-ruby-edition/chapter1/good/customer.rb
+++ refactoring-ruby-edition/chapter1/good/customer.rb
@@ -11,7 +11,7 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
+    frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |rental|
@@ -22,12 +22,21 @@ class Customer
 
       # show figures for this rental
       result += "\t" + rental.movie.title + "\t" + rental.charge.to_s + "\n"
-      total_amount += rental.charge
     end
 
     # add footer lines
-    result += "Amount owed is #{total_amount}\n"
+    result += "Amount owed is #{total_charge}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
+
+    result
+  end
+
+  def total_charge
+    result = 0
+
+    @rentals.each do |rental|
+      result += rental.charge
+    end
 
     result
   end
