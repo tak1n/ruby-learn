c7ea2b7c85ec3352451f51bc1dc7e79e4faeec52
Benny <r3qnbenni@gmail.com>
2014-07-23 12:24:40 +0200

    edited router

--- lotus/lotus_router.rb
+++ lotus/lotus_router.rb
@@ -1,7 +1,7 @@
 require 'lotus/router'
 
 app = Lotus::Router.new do
-  get '/', to: ->(env) { [200, {}, ['Welcome to Lotus::Router!']] }
+    get '/', to: ->(env) { [200, {}, ['Welcome to Lotus::Router!']] }
 end
 
-Rack::Server.start app: app, Port: 3000
+Rack::Server.start app: app
