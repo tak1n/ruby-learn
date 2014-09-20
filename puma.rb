c787dd417e170b3b23bde15fc03b78a3bf08198d
Benny <r3qnbenni@gmail.com>
2014-09-20 23:06:28 +0200

    added server configs

---
+++ puma.rb
@@ -0,0 +1,17 @@
+environment 'production'
+
+threads 16, 16
+workers 2
+
+preload_app!
+
+# bind 'unix:///tmp/sockets/monitr.sock'
+# daemonize
+# pidfile 'tmp/puma.pid'
+# stdout_redirect 'log/puma_stdout.log', 'log/puma_stderr.log', true
+#
+# on_worker_boot do
+#   ActiveSupport.on_load(:active_record) do
+#     ActiveRecord::Base.establish_connection
+#   end
+# end
