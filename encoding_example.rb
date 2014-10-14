5875d3978c06189211c08f2f35ed903d3d7c33dd
Benny <r3qnbenni@gmail.com>
2014-10-14 17:52:22 +0200

    added more encoding examples

--- encoding_example.rb
+++ encoding_example.rb
@@ -1,2 +1,22 @@
 puts Encoding.default_external
 puts Encoding.default_internal
+
+puts __ENCODING__
+
+def show_encoding(str)
+  puts "'#{str}' is #{str.encoding.name}"
+end
+
+show_encoding "cat"
+show_encoding "∂og"
+
+show_encoding /cat/
+show_encoding /∂og/
+
+puts "Greek pi: \u03c0"
+puts "Greek pi: \u{3c0}"
+puts "Greek \u{70 69 3a 20 3c0}"
+puts "\u2026"
+
+p "pi: \u03c0".bytes
+p "pi: π".bytes
