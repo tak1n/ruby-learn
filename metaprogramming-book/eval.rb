94f720f8f8ff7860b071d2e5aa0ad10d53156a7c
Benny Klotz <r3qnbenni@gmail.com>
2014-12-28 23:31:24 +0100

    added comment to class_eval vs instance_eval

--- metaprogramming-book/eval.rb
+++ metaprogramming-book/eval.rb
@@ -1,12 +1,14 @@
 class MyClass
 end
 
+# instance_eval changes current class to #MyClass and self to MyClass
 MyClass.instance_eval do
   def class_method
     puts 'class'
   end
 end
 
+# class_eval changes current class to MyClass and self to MyClass
 MyClass.class_eval do
   def instance_method
     puts 'instance'
