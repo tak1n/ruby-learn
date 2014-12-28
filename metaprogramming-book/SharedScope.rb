ff297ca642276cb1aabb360724b3de2b9e92b2ba
Benny Klotz <r3qnbenni@gmail.com>
2014-12-28 19:49:26 +0100

    updated shared scope example

--- metaprogramming-book/SharedScope.rb
+++ metaprogramming-book/SharedScope.rb
@@ -16,4 +16,22 @@ counter
 inc(4)
 counter
 
-#shared is protected by a Scope Gate but Kernel#counter and Kernel#inc share the same variables because their scope is flattened
+# shared is protected by a Scope Gate but Kernel#counter and Kernel#inc share the same variables because their scope is flattened
+
+def define_methods2
+  shared = 0
+
+  define_method(:counter2) do
+    puts shared
+  end
+
+  define_method(:inc2) do |x|
+    shared += x
+  end
+end
+
+define_methods2
+counter2
+inc2(4)
+counter2
+
