1827171dfca6ceea7c5a23fdeb11dd69c006279d
Benny Klotz <r3qnbenni@gmail.com>
2014-12-28 22:23:14 +0100

    updated singleton methods / class example

--- metaprogramming-book/SingletonMethods.rb
+++ metaprogramming-book/SingletonMethods.rb
@@ -4,9 +4,14 @@ def str.title?
   self.upcase == self
 end
 
-str.title?
-str.method.grep(/title?/)
-str.singleton_methods
+p str.title?
+p str.methods.grep(/title?/)
+p str.singleton_methods
 
-#Singleton methods defined in this way creates an eigenclass for the str object on the fly
-# where this methods then are defined as current class 
+# Singleton methods are instance methods on the Singleton class
+
+p str.class
+p str.singleton_class
+
+p str.class.class
+p str.singleton_class.class
