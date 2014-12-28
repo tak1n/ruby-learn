761c1a8a49646187d90f727d86973940940f4fee
Benny Klotz <r3qnbenni@gmail.com>
2014-12-28 15:18:00 +0100

    update bug_hunt quiz

--- metaprogramming-book/bug_hunt.rb
+++ metaprogramming-book/bug_hunt.rb
@@ -1,7 +1,23 @@
+# class Roulette
+#   def method_missing(name, *args)
+#     person = name.to_s.capitalize
+#     number = 0
+#     3.times do
+#       number = rand(10) + 1
+#       puts "#{number}..."
+#     end
+#
+#     puts "#{person} got a #{number}"
+#   end
+# end
+
 class Roulette
   def method_missing(name, *args)
     person = name.to_s.capitalize
     number = 0
+
+    super unless %w[Bob Frank Bill].include? person
+
     3.times do
       number = rand(10) + 1
       puts "#{number}..."
