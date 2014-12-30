4039b5547abebd1217198bd6cc85078a3757aadb
Benny Klotz <r3qnbenni@gmail.com>
2014-12-30 13:23:53 +0100

    added comments for hook methods

--- metaprogramming-book/Boss Challenge/hook.rb
+++ metaprogramming-book/Boss Challenge/hook.rb
@@ -1,6 +1,13 @@
 module CheckedAttributes
+  # If a class includes CheckedAttributes
+  # instance methods of CheckedAttributes are instance methods for the klass
+  # instance methods of ClassMethods are class methods for the klass
   def self.included(klass)
     klass.extend(ClassMethods)
+  end
+
+  def say
+    puts "hi"
   end
 
   module ClassMethods
