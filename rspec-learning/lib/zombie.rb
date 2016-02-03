f3e77dc4ea6979d9c18b2e1216deed58c7a4eab1
Benny Klotz <benny.klotz92@gmail.com>
2016-02-03 21:09:26 +0100

    update rspec example

--- rspec-learning/lib/zombie.rb
+++ rspec-learning/lib/zombie.rb
@@ -1,7 +1,7 @@
 class Zombie
   attr_accessor :name
-  
+
   def initialize(name)
-  	@name = name
+    @name = name
   end
 end
--- rspec-learning/spec/lib/zombie_spec.rb
+++ rspec-learning/spec/lib/zombie_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
+require_relative '../../lib/zombie.rb'
 
-describe Zombie do
-	it "is named Ash" do
-	  zombie = Zombie.new("Ash")
-	  zombie.name.should eq("Asha")
-	end
+
+RSpec.describe Zombie do
+  it "is named Ash" do
+    zombie = Zombie.new("Ash")
+    expect(zombie.name).to eq("Ash")
+  end
 end
