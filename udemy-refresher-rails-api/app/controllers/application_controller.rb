6d5d6f1332f3c337ec8c13237efd471b1153b72a
Benjamin <benny.klotz92@gmail.com>
2021-12-07 20:44:02 +0100

    Error handling

--- udemy-refresher-rails-api/app/controllers/application_controller.rb
+++ udemy-refresher-rails-api/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  include JsonapiErrorsHandler
+  ErrorMapper.map_errors!({
+      "ActiveRecord::RecordNotFound" =>
+        "JsonapiErrorsHandler::Errors::NotFound"
+  })
+  rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
 end
